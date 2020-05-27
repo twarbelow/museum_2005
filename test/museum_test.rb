d262ad8f37313b5836d7f83817743c1452e9323c
twarbelow <taija.warbelow@google.com>
2020-05-27 09:37:46 -0700

    add test it can have lottery contestants

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -81,4 +81,21 @@ class MuseumTest < Minitest::Test
 
     assert_equal ({@gems_and_minerals => [@patron_1], @dead_sea_scrolls => [@patron_1, @patron_2, @patron_3], @imax => []}), @dmns.patrons_by_exhibit_interest
   end
+
+  def test_it_can_have_lottery_contestants
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_2.add_interest("Dead Sea Scrolls")
+    @patron_3.add_interest("Dead Sea Scrolls")
+
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+
+    assert_equal [@patron_1, @patron_3], @dmns.ticket_lottery_contestants
+  end
 end
