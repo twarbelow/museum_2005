5636de7aa806865dd4628ae811157113ab598442
twarbelow <taija.warbelow@google.com>
2020-05-27 08:13:29 -0700

    set up test it has cost

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -16,4 +16,8 @@ class ExhibitTest < Minitest::Test
   def test_it_has_name
     assert_equal "Gems and Minerals", @exhibit.name
   end
+
+  def test_it_has_cost
+    assert_equal 0, @exhibit.cost
+  end
 end
