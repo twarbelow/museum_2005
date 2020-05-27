87d6c30148617fed27ddf8f3a6a87893364bb7ab
twarbelow <taija.warbelow@google.com>
2020-05-27 08:30:50 -0700

    add to test it has exhibits (add_exhibit method and check of that method)

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -19,5 +19,15 @@ class MuseumTest < Minitest::Test
 
   def test_it_has_exhibits
     assert_equal [], @dmns.exhibits
+
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    @dmns.add_exhibit(gems_and_minerals)
+    @dmns.add_exhibit(dead_sea_scrolls)
+    @dmns.add_exhibit(imax)
+
+    assert_equal [gems_and_minerals, dead_sea_scrolls, imax], @dmns.exhibits
   end
 end
