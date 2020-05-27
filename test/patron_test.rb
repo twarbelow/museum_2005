4238acae946562e364f06a58c22afaf32c6fe65b
twarbelow <taija.warbelow@google.com>
2020-05-27 08:14:56 -0700

    set up test it exists

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -2,3 +2,13 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/exhibit'
 require './lib/patron'
+
+class PatronTest < Minitest::Test
+  def setup
+    @patron_1 = Patron.new("Bob", 20)
+  end
+  
+  def test_it_exists
+    assert_instance_of Patron, @patron_1
+  end
+end
