bd71e3e2507bb739e6b0de5c2665c2de38a78623
twarbelow <taija.warbelow@google.com>
2020-05-27 08:08:42 -0700

    Set up test it exists

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -2,3 +2,14 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/exhibit'
 require './lib/patron'
+
+
+class ExhibitTest < Minitest::Test
+  def setup
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  def test_it_exists
+    assert_instance_of Exhibit, @exhibit
+  end
+end
