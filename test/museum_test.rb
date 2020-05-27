621b6c9be3cc051d43467cfd56bca2688f73727f
twarbelow <taija.warbelow@google.com>
2020-05-27 08:28:16 -0700

    add test it has exhibits, part 1

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -16,4 +16,8 @@ class MuseumTest < Minitest::Test
   def test_it_has_a_name
     assert_equal "Denver Museum of Nature and Science", @dmns.name
   end
+
+  def test_it_has_exhibits
+    assert_equal [], @dmns.exhibits
+  end
 end
