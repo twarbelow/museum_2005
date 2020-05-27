3b9956dac78a660520fa4bb7a9b786e5e21970ad
twarbelow <taija.warbelow@google.com>
2020-05-27 08:53:07 -0700

    move patrons to setup

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/museum'
-require './lib/@patron'
+require './lib/patron'
 require './lib/exhibit'
 
 class MuseumTest < Minitest::Test
@@ -11,6 +11,9 @@ class MuseumTest < Minitest::Test
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
   end
 
   def test_it_exists
@@ -36,11 +39,10 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
 
-    @patron_1 = Patron.new("Bob", 20)
+
     @patron_1.add_interest("Dead Sea Scrolls")
     @patron_1.add_interest("Gems and Minerals")
 
-    @patron_2 = Patron.new("Sally", 20)
     @patron_2.add_interest("IMAX")
 
     assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@patron_1)
