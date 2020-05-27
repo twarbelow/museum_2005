698658624c4be1056f5d133dd700f3f358309081
twarbelow <taija.warbelow@google.com>
2020-05-27 09:10:30 -0700

    fix test it can show patrons by exhibit interest

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -66,12 +66,13 @@ class MuseumTest < Minitest::Test
   end
 
   def test_it_can_show_patrons_by_exhibit_interest
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
     @patron_1.add_interest("Gems and Minerals")
     @patron_1.add_interest("Dead Sea Scrolls")
-
-
     @patron_2.add_interest("Dead Sea Scrolls")
-
     @patron_3.add_interest("Dead Sea Scrolls")
 
     @dmns.admit(@patron_1)
