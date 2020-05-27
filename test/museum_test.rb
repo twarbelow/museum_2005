da695bbd80a4efd02a64761a8147395a6508516f
twarbelow <taija.warbelow@google.com>
2020-05-27 08:33:10 -0700

    move exhibits to setup

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -7,6 +7,10 @@ require './lib/exhibit'
 class MuseumTest < Minitest::Test
   def setup
     @dmns = Museum.new("Denver Museum of Nature and Science")
+
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
   end
 
   def test_it_exists
@@ -20,14 +24,12 @@ class MuseumTest < Minitest::Test
   def test_it_has_exhibits
     assert_equal [], @dmns.exhibits
 
-    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-    imax = Exhibit.new({name: "IMAX",cost: 15})
 
-    @dmns.add_exhibit(gems_and_minerals)
-    @dmns.add_exhibit(dead_sea_scrolls)
-    @dmns.add_exhibit(imax)
 
-    assert_equal [gems_and_minerals, dead_sea_scrolls, imax], @dmns.exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
   end
 end
